bcd8456a91c046363e8759eb4d550358e4fba72e
Ryan Alumbaugh <ralumbaugh@gmail.com>
2020-06-23 14:19:27 -0700

    Adjusted jumping to only work when leaving the ground. Set up speed adjustments for player choices for next push.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,14 +4,47 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
-    
-    void OnCollisionEnter(Collision collisioninfo)
+    void OnCollisionEnter (Collision collisionInfo)
     {
-        if (collisioninfo.collider.tag=="Obstacle")
+        if(collisionInfo.collider.tag == "Obstacle")
         {
-            GetComponent<PlayerMovement>().enabled=false;
+            movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
-            
         }
+        else if(collisionInfo.collider.tag == "Ground")
+        {
+            movement.CanJump = true;
+        }
+        if(collisionInfo.collider.tag == "Wall")
+        {
+            movement.PlayerDirection.rotation = new Quaternion(0f,0f,0f,0f);
+            movement.rb.velocity = new Vector3(0f,0f,0f);
+            movement.rb.useGravity = false;
+            movement.CanClimb = true;
+        }
+    }
+
+    void OnCollisionExit (Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Ground")
+        {
+            movement.CanJump = false;
+        }
+        if(collisionInfo.collider.tag == "Wall")
+        {
+            movement.rb.useGravity = true;
+            movement.CanClimb = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
