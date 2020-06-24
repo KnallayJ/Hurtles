087b674f4c566bff85cc49838aa093bb80b0bae5
KnallayJ <64495164+KnallayJ@users.noreply.github.com>
2020-06-24 11:02:00 -0700

    Added Log, Spawner, new tags

---
+++ Assets/Scripts/LogMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMovement : MonoBehaviour, IPooledLog
+{
+    
+    public float sideForce=1f;
+    // Start is called before the first frame update
+    public void OnLogSpawn()
+    {
+        Vector3 force =new Vector3(sideForce,0,0);
+
+        GetComponent<Rigidbody>().velocity=force;
+    }
+
+    // Update is called once per frame
+  
+}
